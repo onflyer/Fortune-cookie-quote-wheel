a69f9afb8235b44533e69fe24b89e8fd81f8a3db
onflyer <salkaorama@gmail.com>
2023-02-14 09:37:22 +0100

    Initial Commit

---
+++ Wheel of fortune/Wheel_of_fortuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wheel_of_fortuneApp.swift
+//  Wheel of fortune
+//
+//  Created by Aleksandar Milidrag on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Wheel_of_fortuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
